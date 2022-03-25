771fd17bfff25a6ee412cea5f1b3f7c4bbffbd60
Sohail Afzal <sohail.afzal@barqaab.com>
2022-03-25 21:35:22 +0500

    25-03-2022

--- app/src/main/java/com/example/hrms_android_3/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/hrms_android_3/ui/dashboard/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.hrms_android_3.ui.dashboard;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +14,17 @@ import androidx.fragment.app.Fragment;
 
 import com.example.hrms_android_3.classes.PreferenceHelper;
 import com.example.hrms_android_3.databinding.FragmentDashboardBinding;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.util.ArrayList;
 
 
 public class DashboardFragment extends Fragment {
-
+PieChart pieChart;
     private FragmentDashboardBinding binding;
     private PreferenceHelper preferenceHelper;
 
@@ -26,12 +33,30 @@ public class DashboardFragment extends Fragment {
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        pieChart= binding.piechart;
         final TextView textView = binding.textDashboard;
         preferenceHelper = new PreferenceHelper(getContext());
         String userName = preferenceHelper.getName();
         textView.setText(userName + " Welcome to HRMS");
-
+        getAgeChart();
         return root;
+    }
+
+    private void getAgeChart() {
+        ArrayList<PieEntry> records = new ArrayList<>();
+        records.add(new PieEntry(32, "first"));
+        records.add(new PieEntry(12, "second"));
+        records.add(new PieEntry(22, "third"));
+        records.add(new PieEntry(34, "forth"));
+        PieDataSet dataSet = new PieDataSet(records, "Pie Chart Report");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setValueTextSize(22f);
+        PieData pieData = new PieData(dataSet);
+        pieChart.setData(pieData);
+        pieChart.getDescription();
+        pieChart.setCenterText("It is Demo");
+        pieChart.animate();
     }
 
     @Override
